ca347658ca35f0c49c6ae92a8ae1d5a047c7287c
husen <husentod10@gmail.com>
2017-11-30 05:27:20 +0700

    tambah banyak fragment

--- app/src/main/java/com/example/husen/impal/HomeFragment.java
+++ app/src/main/java/com/example/husen/impal/HomeFragment.java
@@ -1,28 +1,83 @@
 package com.example.husen.impal;
 
-import android.support.v4.app.Fragment;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.zip.Inflater;
+import java.util.ArrayList;
+import java.util.List;
+
+import Module.Application;
+import Object.Report;
+import Object.Movie;
+
+import Adapter.RecyclerAdapter;
+
 
 /**
- * Created by husen on 27/11/17.
+ * A simple {@link Fragment} subclass.
  */
+public class HomeFragment extends Fragment {
 
-public class HomeFragment extends Fragment{
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
-    }
+
+    private Application app= null;
+
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getActivity().setTitle("Data View");
     }
+
+    public HomeFragment() {
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View layout= inflater.inflate(R.layout.fragment_home, container, false);
+        RecyclerView rv= (RecyclerView) layout.findViewById(R.id.datalist);
+        Intent intent= getActivity().getIntent();
+        this.app= (Application) intent.getSerializableExtra("application");
+
+        RecyclerAdapter listAdapter= new RecyclerAdapter(prepareData());
+        rv.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager= new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(layoutManager);
+
+
+
+
+
+        return layout;
+    }
+
+    public ArrayList<Movie> prepareData(){
+//        ArrayList<Report> l= app.getRM().getReportList();
+        ArrayList<Movie> l= new ArrayList<>();
+        Report tes= new Report();
+        tes.setiBarcode("ini report tes");
+        tes.setName("ini barang");
+        tes.setPrice(500);
+        tes.setSold(23);
+        tes.setTotal();
+
+
+
+
+        l.add(new Movie("husen", "riri", "1997"));
+        l.add(new Movie("diantar", "kita", "1997"));
+        l.add(new Movie("berdua", "wkwkw", "1997"));
+        l.add(new Movie("anjay", "tes", "1997"));
+        l.add(new Movie("adapter", "list", "1997"));
+        return l;
+
+    }
+
 }
